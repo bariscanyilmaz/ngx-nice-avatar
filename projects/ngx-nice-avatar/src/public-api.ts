17186a468996ea28636243e5691278f2b10615af
Barış Can Yılmaz <baris@bariscanyilmaz.com>
2021-07-01 23:52:15 +0300

    remove lib service

--- projects/ngx-nice-avatar/src/public-api.ts
+++ projects/ngx-nice-avatar/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-nice-avatar
  */
 
-export * from './lib/ngx-nice-avatar.service';
 export * from './lib/ngx-nice-avatar.component';
 export * from './lib/ngx-nice-avatar.module';
